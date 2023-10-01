95446b8aecdf8bcaadde232756d335901dab2a99
Illia Yenchev <illiayenchev@gmail.com>
2023-10-01 12:26:01 +0200

    output chenged

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-};
-
-module.exports = nextConfig;
-module.exports = {
-  output: "export",
   images: {
     unoptimized: true,
   },
 };
+module.exports = nextConfig;
