b881e4f7e71434b8f94fe2bf8753fdf5551d4935
Illia Yenchev <illiayenchev@gmail.com>
2023-10-20 16:14:40 +0200

    car card responsive

--- components/CarCatalogue.js
+++ components/CarCatalogue.js
@@ -4,15 +4,14 @@ import CustomButton from "./CustomButton";
 
 const CarCatalogue = ({ cars }) => {
   const [visibleCars, setVisibleCars] = useState(12);
-  const [buttonScale, setButtonScale] = useState("scale-100");
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    // Показуємо кнопку після завантаження перших CarCard
+    setShowButton(true);
+  }, []);
 
   const handleShowMore = () => {
-    if (visibleCars === 48) {
-      setButtonScale("scale-110");
-      setTimeout(() => {
-        setButtonScale("scale-100");
-      }, 120);
-    }
     setVisibleCars((prevVisibleCars) =>
       prevVisibleCars < 48 ? prevVisibleCars + 12 : prevVisibleCars
     );
@@ -20,29 +19,31 @@ const CarCatalogue = ({ cars }) => {
 
   return (
     <div className="car__catalog-wrapper">
-      <div className="car__catalog-header max-width">
+      <div className="car__catalog-header max-width px-16">
         <h2 className="car__catalog-title text-[40px] font-extrabold">
           Car Catalogue
         </h2>
-        <h3 className="car__catalog-subtitle text-[18px] mb-6">
-          Explore out cars you might like
+        <h3 className="car__catalog-subtitle text-[18px] pb-6">
+          Explore our cars you might like
         </h3>
+        <div className="car__catalog-filter max-width pb-10">Filter</div>
       </div>
-      <div className="car__catalog-filter max-width mb-10">Filter</div>
-      <div className="car__catalog-cards max-width grid grid-cols-4 gap-10 mb-[50px]">
-        {cars.slice(0, visibleCars).map((car) => (
-          <div key={Math.random()}>
+      <div className="car__catalog-cards mx-auto 2xl:w-[1440px] xl:w-[1100px] lg:w-[1000px] md:w-[700px] sm:w-[600px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 sm:gap-4 lg:gap-4 gap-10 mb-[50px]">
+        {cars.slice(0, visibleCars).map((car, index) => (
+          <div className="mx-auto" key={index}>
             <CarCard car={car} />
           </div>
         ))}
       </div>
-      <div className="car__catalog-button">
-        <CustomButton
-          title="Show More"
-          containerStyles={`rounded-full my-10  mx-auto bg-blue-600 transition duration-75 ${buttonScale}`}
-          handleClick={handleShowMore}
-        />
-      </div>
+      {showButton && visibleCars < 48 && (
+        <div className="car__catalog-button">
+          <CustomButton
+            title="Show More"
+            containerStyles="rounded-full my-10 mx-auto bg-blue-600 transition duration-75"
+            handleClick={handleShowMore}
+          />
+        </div>
+      )}
     </div>
   );
 };
